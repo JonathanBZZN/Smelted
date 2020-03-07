484050b43ac7a2d530dbb8c01e242108285168d4
Robbie <robbiejohnwallace@yahoo.co.uk>
2020-03-07 20:52:38 +0000

    Added start menu

--- main.py
+++ main.py
@@ -4,80 +4,82 @@ from score_board import *
 from config import *
 from swords import *
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-done = False
 
-# init player
-player1 = Player(PLAYER_1_CONFIG)
-player2 = Player(PLAYER_2_CONFIG)
-player2.rect.x = 200
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    done = False
 
-furnace = Furnace()
-hammer = Hammer()
-score = ScoreBoard()
-endPoint = EndPoint(score)
-collect = CollectionPoint(Iron)
-grinder = Grinder()
-table = Table()
+    # init player
+    player1 = Player(PLAYER_1_CONFIG)
+    player2 = Player(PLAYER_2_CONFIG)
+    player2.rect.x = 200
 
-# Init sprite groups
-all_sprites = pygame.sprite.Group()
-all_sprites.add(player1)
-all_sprites.add(player2)
-all_sprites.add(collect)
-all_sprites.add(score)
-all_sprites.add(hammer)
-all_sprites.add(grinder)
-all_sprites.add(furnace)
-all_sprites.add(endPoint)
-all_sprites.add(table)
-score.add_item()
-score.add_item()
-score.add_item()
-score.add_item()
+    furnace = Furnace()
+    hammer = Hammer()
+    score = ScoreBoard()
+    endPoint = EndPoint(score)
+    collect = CollectionPoint(Iron)
+    grinder = Grinder()
+    table = Table()
 
-# init game clock
-clock = pygame.time.Clock()
+    # Init sprite groups
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(player1)
+    all_sprites.add(player2)
+    all_sprites.add(collect)
+    all_sprites.add(score)
+    all_sprites.add(hammer)
+    all_sprites.add(grinder)
+    all_sprites.add(furnace)
+    all_sprites.add(endPoint)
+    all_sprites.add(table)
+    score.add_item()
+    score.add_item()
+    score.add_item()
+    score.add_item()
 
-# start music
-pygame.mixer.music.load("Music/ingame.mp3")
-pygame.mixer.music.play(-1)
+    # init game clock
+    clock = pygame.time.Clock()
 
-# game clock
-while not done:
-    # Set screen background
-    screen.fill(BACKGROUND_COLOUR)
+    # start music
+    pygame.mixer.music.load("Music/ingame.mp3")
+    pygame.mixer.music.play(-1)
 
-    # Draw all sprites
-    for entity in all_sprites:
-        if isinstance(entity, Player):
-            screen.blit(entity.surf, entity.rect)
-            # Draw inventory if not none
-            if entity.inventory is not None:
-                screen.blit(entity.inventory.surf, entity.inventory.rect)
-        else:
-            screen.blit(entity.surf, entity.rect)
+    # game clock
+    while not done:
+        # Set screen background
+        screen.fill(BACKGROUND_COLOUR)
 
-        if isinstance(entity, StaticObject) and entity.print_inventory:
-            entity.print(screen)
+        # Draw all sprites
+        for entity in all_sprites:
+            if isinstance(entity, Player):
+                screen.blit(entity.surf, entity.rect)
+                # Draw inventory if not none
+                if entity.inventory is not None:
+                    screen.blit(entity.inventory.surf, entity.inventory.rect)
+            else:
+                screen.blit(entity.surf, entity.rect)
 
-    # Loop through every event in the queue
-    for event in pygame.event.get():
-        # Quiting
-        if event.type == pygame.QUIT:
-            done = True
+            if isinstance(entity, StaticObject) and entity.print_inventory:
+                entity.print(screen)
 
-    # Update players
-    pressed = pygame.key.get_pressed()
-    player1.update(pressed, all_sprites)
-    player2.update(pressed, all_sprites)
-    score.update()
-    furnace.update()
+        # Loop through every event in the queue
+        for event in pygame.event.get():
+            # Quiting
+            if event.type == pygame.QUIT:
+                done = True
 
-    pygame.display.flip()
-    clock.tick(60)
+        # Update players
+        pressed = pygame.key.get_pressed()
+        player1.update(pressed, all_sprites)
+        player2.update(pressed, all_sprites)
+        score.update()
+        furnace.update()
 
-# Clean up
-pygame.quit()
-quit()
+        pygame.display.flip()
+        clock.tick(60)
+
+    # Clean up
+    pygame.quit()
+    quit()
